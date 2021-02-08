1a5cb1f6ec3e9a30b6e5f47e4cc2e5e8a114122e
johnwmcnay <john.w.mcnay@gmail.com>
2021-02-08 14:36:40 -0600

    fresh repo

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('listening:');
+});
